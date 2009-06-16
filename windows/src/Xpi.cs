3c8c2426dcb9cc2ebd356545bf505a084b665295
thekid <thekid@thekid.de>
2009-06-16 19:59:36 +0000

    - Add xpi command
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpadmin/trunk/releases/bin@288 4ada86be-2990-db11-8b6f-0050450081cd

---
+++ windows/src/Xpi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net.XpFramework.Runner
+{
+    class Xpi : BaseRunner
+    {
+
+        static void Main(string[] args)
+        {
+            Execute("class", "xp.install.Runner", new string[] { "." }, args);
+        }
+    }
+}
