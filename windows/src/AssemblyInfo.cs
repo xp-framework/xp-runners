b3806ec3afdfa216dd2912e80e656733cd4caf0a
thekid <thekid@thekid.de>
2010-03-14 19:50:04 +0000

    - Embed version & product information
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpadmin/trunk/releases/bin@437 4ada86be-2990-db11-8b6f-0050450081cd

---
+++ windows/src/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("XP-Framework Team")]
+[assembly: AssemblyProduct("XP Runners")]
+[assembly: AssemblyTitle("XP Runner")]
+[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2010")]
+[assembly: AssemblyVersion("2.0.10.436")]
