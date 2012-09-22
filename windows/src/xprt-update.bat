a4a12f8b879170f8c644a6591af7b84a81ef4200
Timm Friebe <thekid@thekid.de>
2012-09-22 13:10:48 +0200

    - Create temporary directory inside current dir if TEMP/TMP not set

--- windows/src/xprt-update.bat
+++ windows/src/xprt-update.bat
@@ -1,15 +1,23 @@
 @echo off
 
-set TARGET=%TMP%\.xp
+if not "%TEMP%"=="" (
+  set TARGET=%TEMP%\.xp
+) else (
+  if not "%TMP%"=="" (
+    set TARGET=%TMP%\.xp
+  ) else (
+    set TARGET=%~p0\.xp
+  )
+)
 
 echo ===^> Downloading Windows runners to %TARGET%
-xpi download bin/windows %TARGET%
+xpi download bin/windows "%TARGET%"
 if ERRORLEVEL 1 goto end
 
 echo ===^> Extracting into %~p0
-xcopy /y /q %TARGET%\* "%~p0"
+xcopy /y /q "%TARGET%\*" "%~p0"
 
-rmdir /s /q %TARGET%
+rmdir /s /q "%TARGET%"
 echo ===^> Done, runners have been updated
 
 :end
