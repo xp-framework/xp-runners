133eee22aa6adb5dc8f1b866c9a5330b20b1b404
Timm Friebe <thekid@thekid.de>
2012-09-22 13:17:12 +0200

    - Assign RUNNERS variable to current path # Should prevent error message after xcopy

--- windows/src/xprt-update.bat
+++ windows/src/xprt-update.bat
@@ -9,13 +9,14 @@ if not "%TEMP%"=="" (
     set DOWNLOAD=%~p0\.xp
   )
 )
+set RUNNERS=%~p0
 
 echo ===^> Downloading Windows runners to %DOWNLOAD%
 xpi download bin/windows "%DOWNLOAD%"
 if ERRORLEVEL 1 goto end
 
-echo ===^> Extracting into %~p0
-xcopy /y /q "%DOWNLOAD%\*" "%~p0"
+echo ===^> Extracting into %RUNNERS%
+xcopy /y /q "%DOWNLOAD%\*" "%RUNNERS%"
 
 rmdir /s /q "%DOWNLOAD%"
 echo ===^> Done, runners have been updated
