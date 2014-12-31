d59466ec27a0bd84821db12bd64f58fc742a2325
Timm Friebe <thekid@thekid.de>
2014-12-31 16:51:08 +0100

    Fix function name

--- shared/src/class-path.php
+++ shared/src/class-path.php
@@ -1,6 +1,6 @@
 <?php namespace xp;
 
-list($bootstrap, $paths)= classpath(scan(array('.'), $home), function() {
+list($bootstrap, $paths)= bootstrap(scan(array('.'), $home), function() {
   list($use, $inc)= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
   return array_merge(
     scan(array_unique(explode(PATH_SEPARATOR, substr($use, 2))), $home),
