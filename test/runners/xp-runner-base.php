7509040b0620822988b7aea1b03f63d6744f17b4
Timm Friebe <thekid@thekid.de>
2015-01-06 19:08:29 +0100

    Test BSD runners if on BSD

--- test/runners/xp-runner-base.php
+++ test/runners/xp-runner-base.php
@@ -8,6 +8,10 @@ return [
     if (0 === strncasecmp(PHP_OS, 'Win', 3)) {
       $ar->extract($path->compose(__DIR__, '../../windows.ar'), 'xp.exe', $this->tmp);
       $this->exe= $path->compose($this->tmp, 'xp.exe');
+    } else if (preg_match('/^([A-Z]+)BSD/i', PHP_OS)) {
+      $ar->extract($path->compose(__DIR__, '../../bsd.ar'), 'xp', $this->tmp);
+      $this->exe= $path->compose($this->tmp, 'xp');
+      chmod($this->exe, 0700);
     } else {
       $ar->extract($path->compose(__DIR__, '../../unix.ar'), 'xp', $this->tmp);
       $this->exe= $path->compose($this->tmp, 'xp');
