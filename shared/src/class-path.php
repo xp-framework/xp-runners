b0f5c19d4401cb874c8cd249025ec2e61df8330b
Timm Friebe <thekid@thekid.de>
2015-01-06 19:03:42 +0100

    Fix home dir not being expanded inside global paths

--- shared/src/class-path.php
+++ shared/src/class-path.php
@@ -1,6 +1,6 @@
 <?php namespace xp;
 
-list($bootstrap, $paths)= bootstrap(scan(array($cwd), $home), function() {
+list($bootstrap, $paths)= bootstrap(scan(array($cwd), $home), function() use($home) {
   list($use, $inc)= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
   return array_merge(
     scan(array_unique(explode(PATH_SEPARATOR, substr($use, 2))), $home),
