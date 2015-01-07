7e181029f6829d4048bcc5cdf10d2c23b3ea5457
Timm Friebe <thekid@thekid.de>
2015-01-07 23:09:08 +0100

    Try fixing exes compiled on a box with .NET v4.5 not working on boxes with v4.0
    After a lot of Googling, System.Runtime.Versioning.TargetFramework seems
    to be the solution to this. I will verify this tomorrow in the office
    
    See also #14

--- windows/src/AssemblyInfo.cs
+++ windows/src/AssemblyInfo.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("XP-Framework Team")]
 [assembly: AssemblyProduct("XP Runners")]
 [assembly: AssemblyTitle("XP Runner")]
-[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2010")]
-[assembly: AssemblyVersion("2.0.10.436")]
+[assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2015")]
+[assembly: AssemblyVersion("5.2.0.436")]
+[assembly: TargetFramework(".NETFramework,Version=v4.0")]
