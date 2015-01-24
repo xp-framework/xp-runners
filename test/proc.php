799794c03045a0cc96f19ee27976815a2e1c879f
Timm Friebe <thekid@thekid.de>
2015-01-24 11:34:42 +0100

    Make it possible to supply working directory for process execution

--- test/proc.php
+++ test/proc.php
@@ -3,14 +3,14 @@
 class Proc {
   private static $opt= ['bypass_shell' => false];
 
-  public function execute($exe, array $args, array $env= []) {
+  public function execute($exe, array $args, array $env= [], $wd= null) {
     $descriptors= [
       0 => ['pipe', 'r'],
       1 => ['pipe', 'w'],
       2 => STDERR
     ];
 
-    $p= proc_open($exe.' '.implode(' ', $args), $descriptors, $pipes, getcwd(), $env, self::$opt);
+    $p= proc_open($exe.' '.implode(' ', $args), $descriptors, $pipes, $wd ?: getcwd(), $env, self::$opt);
     fclose($pipes[0]);
     $out= explode("\n", trim(stream_get_contents($pipes[1])));
     fclose($pipes[1]);
