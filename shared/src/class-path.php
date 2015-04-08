91ae30f5363d4bd9b5ff8c6117a31b5924502b4b
Johannes <johnis.adresse@gmx.de>
2015-04-08 11:25:30 +0200

    Update class-path.php
    Shorter version

--- shared/src/class-path.php
+++ shared/src/class-path.php
@@ -2,12 +2,9 @@
 
 list($bootstrap, $paths)= bootstrap(scan(array($cwd), $home), function() use($home) {
   $parts= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
-  $use= $parts[0];
-  $inc= isset($parts[1]) ? $parts[1]  : '';
-  
-  $paths= scan(array_unique(explode(PATH_SEPARATOR, substr($use, 2))), $home);
-  if ($inc != '') {
-    $paths= array_merge($paths, array_map('xp\path', explode(PATH_SEPARATOR, $inc)));
+  $paths= scan(array_unique(explode(PATH_SEPARATOR, substr($parts[0], 2))), $home);
+  if (isset($parts[1]) && $parts[1] != '') {
+    $paths= array_merge($paths, array_map('xp\path', explode(PATH_SEPARATOR, $parts[1])));
   }
   return $paths;
 });
