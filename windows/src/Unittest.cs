b44b980b8447f8e4903b311316cd3904e0173d99
Timm Friebe <thekid@thekid.de>
2015-05-18 23:12:38 +0200

    Implement "unittest -w" on Windows
    Watches the given directory and re-runs the unittests (in a fresh
    process!) as soon as files change inside the directory
    
    Example: $ unittest -w src src/test/php

--- windows/src/Unittest.cs
+++ windows/src/Unittest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace Net.XpFramework.Runner
@@ -8,7 +9,44 @@ namespace Net.XpFramework.Runner
 
         static void Main(string[] args)
         {
-            Execute("class", "xp.unittest.Runner", new string[] { "." }, args);
+            var watch = Array.IndexOf(args, "-w");
+            if (watch < 0)
+            {
+                Execute("class", "xp.unittest.Runner", new string[] { "." }, args);
+            }
+            else
+            {
+                var remaining = new string[args.Length - 2];
+                Array.Copy(args, remaining, watch);
+                Array.Copy(args, watch + 2, remaining, watch, args.Length - watch - 2);
+
+                var watcher = new FileSystemWatcher {
+                  Path = args[watch + 1],
+                  IncludeSubdirectories = true,
+                  Filter = "*.*"
+                };
+
+                using (watcher) 
+                {
+                    watcher.EnableRaisingEvents = true;
+
+                    do
+                    {
+                        try
+                        {
+                            Executor.Execute(Paths.DirName(Paths.Binary()), "class", "xp.unittest.Runner", new string[] { "." }, remaining);
+                        }
+                        catch (Exception e) 
+                        {
+                            Console.Error.WriteLine("*** " + e.GetType() + ": " + e.Message);
+                            Environment.Exit(0xFF);
+                        }
+
+                        watcher.WaitForChanged(WatcherChangeTypes.Changed);
+                    } while (true);
+                }
+                Environment.Exit(0);
+            }
         }
     }
 }
