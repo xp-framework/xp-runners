0cff7d0c8f162e64c6ec2d3a8f474f893926427e
Timm Friebe <thekid@thekid.de>
2015-05-18 23:36:17 +0200

    Don't go into watch mode if first run exits with startup error
    Requires a patch to the unittest runner in order to make these

--- windows/src/Unittest.cs
+++ windows/src/Unittest.cs
@@ -7,43 +7,52 @@ namespace Net.XpFramework.Runner
     class Unittest : BaseRunner
     {
 
+        /// Runs the command and returns the exitcode
+        private static int Run(string[] args)
+        {
+            try
+            {
+                return Executor.Execute(Paths.DirName(Paths.Binary()), "class", "xp.unittest.Runner", new string[] { "." }, args);
+            }
+            catch (Exception e) 
+            {
+                Console.Error.WriteLine("*** " + e.GetType() + ": " + e.Message);
+                return -1;
+            }
+        }
+
         static void Main(string[] args)
         {
             var watch = Array.IndexOf(args, "-w");
             if (watch < 0)
             {
-                Execute("class", "xp.unittest.Runner", new string[] { "." }, args);
+                Environment.Exit(Run(args));
             }
             else
             {
-                var remaining = new string[args.Length - 2];
-                Array.Copy(args, remaining, watch);
-                Array.Copy(args, watch + 2, remaining, watch, args.Length - watch - 2);
+                // First run may not exit with a startup or argument error
+                var code = Run(args);
+                if (-1 == code)
+                {
+                    Environment.Exit(0xFF);
+                }
+                else if (2 == code)
+                {
+                    Environment.Exit(2);
+                }
 
                 var watcher = new FileSystemWatcher {
                   Path = args[watch + 1],
                   IncludeSubdirectories = true,
                   Filter = "*.*"
                 };
-
                 using (watcher) 
                 {
                     watcher.EnableRaisingEvents = true;
-
-                    do
+                    while (!watcher.WaitForChanged(WatcherChangeTypes.Changed).TimedOut)
                     {
-                        try
-                        {
-                            Executor.Execute(Paths.DirName(Paths.Binary()), "class", "xp.unittest.Runner", new string[] { "." }, remaining);
-                        }
-                        catch (Exception e) 
-                        {
-                            Console.Error.WriteLine("*** " + e.GetType() + ": " + e.Message);
-                            Environment.Exit(0xFF);
-                        }
-
-                        watcher.WaitForChanged(WatcherChangeTypes.Changed);
-                    } while (true);
+                        Run(args);
+                    }
                 }
                 Environment.Exit(0);
             }
