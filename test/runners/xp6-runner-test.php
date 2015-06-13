e82aefb8215b05184ded649e2ed3606eb43121b0
Timm Friebe <thekid@thekid.de>
2015-06-13 15:38:41 +0200

    Bundle XP 6.3.1 to make PHP7 work

--- test/runners/xp6-runner-test.php
+++ test/runners/xp6-runner-test.php
@@ -9,11 +9,11 @@ $base= require 'xp-runner-base.php';
 exit($test->run(array_merge($base, [
   '@prepare' => function() use($ar, $path) {
     $ar->extract($path->compose(__DIR__, '../../unix.ar'), 'class-main.php', $this->tmp);
-    file_put_contents($this->boot, $path->compose(__DIR__, 'xp-rt-6.0.0.xar'));
+    file_put_contents($this->boot, $path->compose(__DIR__, 'xp-rt-6.3.1.xar'));
   },
 
   '@version' => function() {
-    return '6.0.0';
+    return '6.3.1-dev';
   },
 
   'uncaught throwables' => function() use($path, $proc) {
