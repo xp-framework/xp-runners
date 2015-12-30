64436f587b2db290028e21013e6cf32b73c7fd41
Timm Friebe <thekid@thekid.de>
2015-12-30 12:47:11 +0100

    Implement searching for modules in "USE_XP"

--- shared/src/class-path.php
+++ shared/src/class-path.php
@@ -2,11 +2,20 @@
 
 list($bootstrap, $include)= bootstrap(scan(array($cwd), $home), function() use($home) {
   $parts= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
-  $paths= scan(array_unique(explode(PATH_SEPARATOR, substr($parts[0], 2))), $home);
+  $use= array_unique(explode(PATH_SEPARATOR, substr($parts[0], 2)));
+  $paths= scan($use, $home);
   if (isset($parts[1]) && $parts[1] != '') {
     foreach (explode(PATH_SEPARATOR, $parts[1]) as $path) {
       if ('%' === $path{0}) {
-        $paths= array_merge($paths, scan(array(substr($path, 1)), $home));
+        $module= substr($path, 1);
+        foreach (array_merge(array($cwd ?: '.'), $use) as $search) {
+          $dir= $search.DIRECTORY_SEPARATOR.$module;
+          if (is_dir($dir)) {
+            $paths= array_merge($paths, scan(array($dir), $home));
+            continue 2;
+          }
+        }
+        throw new \Exception('[bootstrap] Module ['.$module.'] not found in '.$parts[0]);
       } else {
         $paths[]= path($path);
       }
