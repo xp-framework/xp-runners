fd4322ef304c1080bc6e649e0c47edd5c976b696
Timm Friebe <thekid@thekid.de>
2016-01-10 21:38:01 +0100

    Check local module, then classpath entries before other modules
    This way, we can say `xp -cp ./xp-6.10.0/xp-6.10.0.xar` and have it
    work the same as putting that into the local .pth file

--- shared/src/bootstrap.php
+++ shared/src/bootstrap.php
@@ -8,10 +8,20 @@ function bootstrap($cwd, $home) {
     'local'    => array(),
     'files'    => array()
   );
+  $parts= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
+
+  // Check local module first
+  scanpath($result, pathfiles($cwd), $cwd, $home);
+
+  // We rely classpath always includes "." at the beginning
+  if (isset($parts[1])) {
+    foreach (explode(PATH_SEPARATOR, substr($parts[1], 2)) as $path) {
+      scanpath($result, array($path), $cwd, $home);
+    }
+  }
 
   // We rely modules always includes "." at the beginning
-  $parts= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
-  foreach (array_unique(explode(PATH_SEPARATOR, $parts[0])) as $path) {
+  foreach (array_unique(explode(PATH_SEPARATOR, substr($parts[0], 2))) as $path) {
     if ('' === $path) {
       continue;
     } else if ('~' === $path{0}) {
@@ -20,13 +30,7 @@ function bootstrap($cwd, $home) {
     scanpath($result, pathfiles($path), $path, $home);
   }
 
-  // We rely classpath always includes "." at the beginning
-  if (isset($parts[1])) {
-    foreach (explode(PATH_SEPARATOR, substr($parts[1], 2)) as $path) {
-      scanpath($result, array($path), $cwd, $home);  
-    }
-  }
-
+  // Always add current directory
   $result['local'][]= path($cwd);
   return $result;
 }
