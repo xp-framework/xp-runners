aa5b973d4561d3be0c102097045c7f2e0ff72dfa
Timm Friebe <thekid@thekid.de>
2016-06-04 16:52:00 +0200

    Make unencoded command line arguments work on HHVM

--- shared/src/class-main.php
+++ shared/src/class-main.php
@@ -75,7 +75,8 @@ PHP_VERSION < '5.6' && iconv_set_encoding('internal_encoding', \xp::ENCODING);
 array_shift($_SERVER['argv']);
 array_shift($argv);
 if (\xp::ENCODING !== get_cfg_var('encoding')) foreach ($argv as $i => $val) {
-  $_SERVER['argv'][$i]= $argv[$i]= iconv('utf-7', \xp::ENCODING, $val);
+  $decoded= iconv('utf-7', \xp::ENCODING, $val);
+  $_SERVER['argv'][$i]= $argv[$i]= false === $decoded ? $val : $decoded;
 }
 
 $ext= substr($argv[0], -4, 4);
